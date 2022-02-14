76af2f6e0ff44dee49815b45cb4700732fcf84a5
Ishkhan Terzian <86164085+IshkhanTerzian@users.noreply.github.com>
2022-02-13 19:53:13 -0500

    Add files via upload
    Loading the files for this particular project

---
+++ Hamilton Google Maps - Galleries & Museums/a1_Ishkhan_terzian/Assignmnet 1/museums_galleries.js
@@ -0,0 +1,85 @@
+/*
+  StAuth10244: I Ishkhan Terzian, 001216827 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else
+*/
+const museums_galleries = [
+  {
+    "NAME": "Griffin House National Historic Site",
+    "LONGITUDE": -80.00293884269504,
+    "LATITUDE": 43.23569681981193,
+    "ADDRESS": "733 Mineral Springs Road",
+    "TYPE": "gallerie",
+    "URL": "https://www.hamilton.ca/attractions/hamilton-civic-museums/griffin-house-national-historic-site"
+  },
+  {
+    "NAME": "Fieldcote Memorial Park and Museum",
+    "LONGITUDE": -79.9800396448889,
+    "LATITUDE": 43.22623097578291,
+    "ADDRESS": "64 Sulphur Springs Road",
+    "TYPE": "museum",
+    "URL": "https://www.hamilton.ca/attractions/hamilton-civic-museums/fieldcote-memorial-park-museum" 
+  }, 
+  {
+    "NAME": "Canadian Warplane Heritage Museum",
+    "LONGITUDE": -79.92520011963968,
+    "LATITUDE": 43.15945178905186,
+    "ADDRESS": "9280 Airport Road",
+    "TYPE": "museum",
+    "URL": "https://www.warplane.com/"
+  },
+  {
+    "NAME": "Dundurn National Historic Site",
+    "LONGITUDE": -79.88418485840054,
+    "LATITUDE": 43.26947046602251,
+    "ADDRESS": "610 York Boulevard",
+    "TYPE": "gallerie",
+    "URL": "https://www.hamilton.ca/attractions/hamilton-civic-museums/dundurn-national-historic-site"
+  },
+  {
+    "NAME": "Art Gallery of Hamilton",
+    "LONGITUDE": -79.87240736848725,
+    "LATITUDE": 43.2572413238943,
+    "ADDRESS": "123 King Street West",
+    "TYPE": "gallerie",
+    "URL": "https://www.artgalleryofhamilton.com/"
+  },
+  {
+    "NAME": "Canadian Football Hall of Fame and Museum",
+    "LONGITUDE": -79.83122957246466,
+    "LATITUDE": 43.251825443398296,
+    "ADDRESS": "64 Melrose Avenue North",
+    "TYPE": "museum",
+    "URL": "https://www.cfhof.ca/"
+  },
+  {
+    "NAME": "Whitehern Historic House and Garden National Histo",
+    "LONGITUDE": -79.87201710746996,
+    "LATITUDE": 43.254584683398264,
+    "ADDRESS": "741 Jackson Street West",
+    "TYPE": "gallerie",
+    "URL": "https://www.hamilton.ca/attractions/hamilton-civic-museums/whitehern-historic-house-garden-national-historic-site" 
+  },
+  {
+    "NAME": "Hamilton Children's Museum",
+    "LONGITUDE": -79.82575100001065,
+    "LATITUDE": 43.243527780248904,
+    "ADDRESS": "1072 Main Street East",
+    "TYPE": "museum",
+    "URL": "https://www.hamilton.ca/attractions/hamilton-civic-museums/hamilton-childrens-museum"
+  },
+  {
+    "NAME": "Hamilton Museum of Steam and Technology National H",
+    "LONGITUDE": -79.77199666931459,
+    "LATITUDE": 43.2565160903868,
+    "ADDRESS": "900 Woodward Avenue",
+    "TYPE": "museum",
+    "URL": "https://www.hamilton.ca/attractions/hamilton-civic-museums/hamilton-museum-steam-technology-national-historic-site"
+  },
+  {
+    "NAME": "Battlefield House Museum and Park National Histori",
+    "LONGITUDE": -79.76609660077854,
+    "LATITUDE": 43.2176066549465,
+    "ADDRESS": "77 King Street West",
+    "TYPE": "gallerie",
+    "URL": "https://www.hamilton.ca/attractions/hamilton-civic-museums/battlefield-house-museum-park-national-historic-site"
+  }
+]
---
+++ Hamilton Google Maps - Galleries & Museums/a1_Ishkhan_terzian/Assignmnet 1/waterfalls.js
@@ -0,0 +1,115 @@
+/*
+  StAuth10244: I Ishkhan Terzian, 001216827 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else
+*/
+const waterfall = [
+  {
+    "HEIGHT_IN_M": 4.0,
+    "WIDTH_IN_M": 1.0,
+    "OWNERSHIP": "private",
+    "OBJECTID": 12,
+    "NAME": "Grand Cascade",
+    "COMMUNITY": "Stoney Creek",
+    "RANKING" : "C",
+    "LONGITUDE": -79.6493195000052,
+    "LATITUDE": 43.19701635644114
+  },
+  {
+    "HEIGHT_IN_M": 4.0,
+    "WIDTH_IN_M": 4.0,
+    "OWNERSHIP": "private",
+    "OBJECTID": 2,
+    "NAME": "Lower Sydenham Falls",
+    "COMMUNITY": "Dundas",
+    "RANKING" : "B",
+    "LONGITUDE": -79.9583534540946,
+    "LATITUDE": 43.273959676196455
+  }, 
+  {
+    "HEIGHT_IN_M": 7.0,
+    "WIDTH_IN_M": 4.0,
+    "OWNERSHIP": "private",
+    "OBJECTID": 3,
+    "NAME": "Mill Falls",
+    "COMMUNITY": "Ancaster",
+    "RANKING" : "B",
+    "LONGITUDE": -79.97389756138983,
+    "LATITUDE": 43.2333543686237
+  },
+  {
+    "HEIGHT_IN_M": 19.8,
+    "WIDTH_IN_M": 1.0,
+    "OWNERSHIP": "public",
+    "OBJECTID": 17,
+    "NAME": "West Iroquoia Falls",
+    "COMMUNITY": "Hamilton",
+    "RANKING" : "B",
+    "LONGITUDE": -79.94049199184443,
+    "LATITUDE": 43.24439564651954
+  },
+  {
+    "HEIGHT_IN_M": 18.0,
+    "WIDTH_IN_M": 9.0,
+    "OWNERSHIP": "public",
+    "OBJECTID": 33,
+    "NAME": "Chedoke Falls",
+    "COMMUNITY": "Hamilton",
+    "RANKING" : "A",
+    "LONGITUDE": -79.9006560601081,
+    "LATITUDE": 43.241723548041556
+  },
+  {
+    "HEIGHT_IN_M": 7.0,
+    "WIDTH_IN_M": 3.0,
+    "OWNERSHIP": "public",
+    "OBJECTID": 74,
+    "NAME": "Little Falls",
+    "COMMUNITY": "Ancaster",
+    "RANKING" : "C",
+    "LONGITUDE": -79.9655785321566,
+    "LATITUDE": 43.239546659921814
+  },
+  {
+    "HEIGHT_IN_M": 22.0,
+    "WIDTH_IN_M": 30.0,
+    "OWNERSHIP": "public",
+    "OBJECTID": 61,
+    "NAME": "Webster's Falls",
+    "COMMUNITY": "Flamborough",
+    "RANKING" : "A",
+    "LONGITUDE": -79.98091775995385,
+    "LATITUDE": 43.276170962051644
+  },
+  {
+    "HEIGHT_IN_M": 3.0,
+    "WIDTH_IN_M": 3.0,
+    "OWNERSHIP": "public",
+    "OBJECTID": 58,
+    "NAME": "Upper Glendale Falls",
+    "COMMUNITY": "Hamilton",
+    "RANKING" : "C",
+    "LONGITUDE": -79.8096628664032,
+    "LATITUDE": 43.20227305588562
+  },
+  {
+    "HEIGHT_IN_M": 14.0,
+    "WIDTH_IN_M": 2.5,
+    "OWNERSHIP": "private",
+    "OBJECTID": 9,
+    "NAME": "Veevers Falls",
+    "COMMUNITY": "Hamilton",
+    "RANKING" : "C",
+    "LONGITUDE": -79.78090203240046,
+    "LATITUDE": 43.20819236184315
+  },
+  {
+    "HEIGHT_IN_M": 9.0,
+    "WIDTH_IN_M": 3.0,
+    "OWNERSHIP": "private",
+    "OBJECTID": 16,
+    "NAME": "Lewis Rd. West Falls",
+    "COMMUNITY": "Hamilton",
+    "RANKING" : "C",
+    "LONGITUDE": -79.66581822715989,
+    "LATITUDE": 43.200685764461774
+  }
+]
